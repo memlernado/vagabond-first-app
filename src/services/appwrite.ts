039458678beba1d3f9814965350da7736fdf7968
Santiago Gonzalez <sagodi97@gmail.com>
2023-03-23 15:13:04 +0100

    feat: initial appwrite client setup

---
+++ src/services/appwrite.ts
@@ -0,0 +1,28 @@
+import { Client, Account, Databases } from "appwrite";
+import type { ITodo } from "../types/todo";
+
+const DB = import.meta.env.VITE_DB_ID;
+const TODOS_COLLECTION = import.meta.env.VITE_TODOS_COLLECTION_ID;
+
+const client = new Client();
+
+client
+  .setEndpoint("https://appwrite.vagabondfirst.com/v1")
+  .setProject(import.meta.env.VITE_PROJECT_ID);
+
+export const accountService = new Account(client);
+
+const db = new Databases(client);
+
+export const todosService = {
+  listTodos: async () => {
+    const res = await db.listDocuments<ITodo>(DB, TODOS_COLLECTION);
+    return res.documents;
+  },
+  createTodo: async (id: string, todo: Pick<ITodo, "title">) =>
+    await db.createDocument(DB, TODOS_COLLECTION, id, todo),
+  updateTodo: async (
+    id: string,
+    todo: Partial<Pick<ITodo, "title" | "isCompleted">>
+  ) => await db.updateDocument(DB, TODOS_COLLECTION, id, todo),
+};
