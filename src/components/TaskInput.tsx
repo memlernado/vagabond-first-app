4bd11e3c5c1910428024f9a251e71b1b48127f3f
Santiago Gonzalez <sagodi97@gmail.com>
2023-03-23 15:14:30 +0100

    feat: add input bar component

---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import { Anchor } from "react-feather";
+import { addTodo } from "../store/features/todosSlice";
+import { useState } from "react";
+import { useAppDispatch } from "../store";
+
+const Form = styled.form`
+  padding: 10px;
+  display: grid;
+  grid-template-columns: 1fr 18px;
+  position: relative;
+  width: 100%;
+  max-width: 600px;
+  background-color: #ffffff;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  overflow: hidden;
+  font-size: 18px;
+  border: ${({ theme }) =>
+    theme.id === "light"
+      ? `1px solid ${theme.color}`
+      : "1px solid transparent"};
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+`;
+
+const Submit = styled.button`
+  padding: 0;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & svg {
+    width: 18px;
+    height: auto;
+  }
+`;
+
+const TaskInput: React.FC = () => {
+  const [newTodo, setNewTodo] = useState("");
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newTodo) {
+      dispatch(addTodo(newTodo));
+      setNewTodo("");
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit} noValidate>
+      <Input
+        type="text"
+        placeholder="What are you crushing today?"
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+        enterKeyHint="search"
+        spellCheck="false"
+        maxLength={280}
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.currentTarget.value)}
+      />
+      <Submit>
+        <Anchor />
+      </Submit>
+    </Form>
+  );
+};
+
+export default TaskInput;
